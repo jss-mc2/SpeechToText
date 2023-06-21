02c0ebe575702dddfd1b93b35fc02a7309083d7b
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-22 04:35:42 +0700

    Initial Commit

---
+++ SpeechToText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeechToText
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 22/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
